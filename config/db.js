26e2530711b3d238639c10b988e3b4abdce4d1ac
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-04-10 16:45:28 +0800

    made a fallback value when there is no .env

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
+	host: process.env.DB_HOST || "127.0.0.1",
+	user: process.env.DB_USER || "root",
+	password: process.env.DB_PASSWORD || "",
+	database: process.env.DB_NAME || "studentportal",
 });
 
 db.connect((err) => {
